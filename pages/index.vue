ee274d40a8f9046bcab20eaf69e98b91aede9ebc
Arturs <mani.servisi@gmail.com>
2022-05-09 22:32:09 +0100

    Adding NetlifyCMS

--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,11 @@ export default {
     name: 'Home',
     middleware({ redirect }) {
         return redirect('/interiors');
-    }
+    },
+    head() {
+        return {
+            script: [{ src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }],
+        };
+    },
 }
 </script>
